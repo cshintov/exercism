7a459e8433d135fc7bd55d7800ea818f6ec1ac68
Shinto C V <cshintov@gmail.com>
2023-07-18 20:44:04 +0530

    refactor kitchen calculator

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -9,35 +9,27 @@ defmodule KitchenCalculator do
   def to_milliliter({:teaspoon, volume}), do: {:milliliter, volume * 5}
   def to_milliliter({:tablespoon, volume}), do: {:milliliter, volume * 15}
 
-  def from_milliliter_helper(volume_pair, in_ml) do
-    unit = volume_pair
-      |> to_milliliter
-      |> get_volume
-      |> Kernel./ in_ml
+  def from_milliliter({unit, volume}, :cup) when unit == :milliliter do
+    {:cup, volume / 240}
   end
 
-  def from_milliliter(volume_pair, :cup) do
-    {:cup, from_milliliter_helper(volume_pair, 240)}
+  def from_milliliter({unit, volume}, :milliliter) when unit == :milliliter do
+    {:milliliter, volume}
   end
 
-  def from_milliliter(volume_pair, :milliliter) do
-    {:milliliter, get_volume(volume_pair)}
+  def from_milliliter({unit, volume}, :fluid_ounce) when unit == :milliliter do
+    {:fluid_ounce, volume / 30}
   end
 
-  def from_milliliter(volume_pair, :fluid_ounce) do
-    {:fluid_ounce, from_milliliter_helper(volume_pair, 30)}
+  def from_milliliter({unit, volume}, :teaspoon) when unit == :milliliter do
+    {:teaspoon, volume / 5}
   end
 
-  def from_milliliter(volume_pair, :teaspoon) do
-    {:teaspoon, from_milliliter_helper(volume_pair, 5)}
-  end
-
-  def from_milliliter(volume_pair, :tablespoon) do
-    {:tablespoon, from_milliliter_helper(volume_pair, 15)}
+  def from_milliliter({unit, volume}, :tablespoon) when unit == :milliliter do
+    {:tablespoon, volume / 15}
   end
 
   def convert(volume_pair, unit) do
-    to_milliliter(volume_pair)
-      |> from_milliliter unit
+    volume_pair |> to_milliliter() |> from_milliliter(unit)
   end
 end
