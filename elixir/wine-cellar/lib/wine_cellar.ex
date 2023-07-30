599f498cf6ef7fb9eef169f9956ac835289c6c96
Shinto C V <cshintov@gmail.com>
2023-07-30 16:56:00 +0530

    refactor wine cellar

--- elixir/wine-cellar/lib/wine_cellar.ex
+++ elixir/wine-cellar/lib/wine_cellar.ex
@@ -8,55 +8,28 @@ defmodule WineCellar do
   end
 
   def filter(cellar, color, opts \\ [])
+  def filter(cellar, color, []) do
+    cellar
+    |> Keyword.get_values(color)
+  end
+
   def filter(cellar, color, year: year) do
     cellar
-    |> filter_by_color(color)
-    |> IO.inspect(label: "\nfilter by year: ")
+    |> Keyword.get_values(color)
     |> filter_by_year(year)
-    |> IO.inspect(label: "\nfilter by year: ")
   end
 
   def filter(cellar, color, country: country) do
     cellar
-    |> filter_by_color(color)
-    |> IO.inspect(label: "\nfilter by country: ")
+    |> Keyword.get_values(color)
     |> filter_by_country(country)
-    |> IO.inspect(label: "\nfilter by country: ")
   end
 
-  def filter(cellar, color, year: year, country: country) do
+  def filter(cellar, color, opts) do
     cellar
-    |> filter_by_color(color)
-    |> IO.inspect(label: "\nfilter by year and country: ")
-    |> filter_by_year(year)
-    |> IO.inspect(label: "\nfilter by year and country: ")
-    |> filter_by_country(country)
-    |> IO.inspect(label: "\nfilter by year and country: ")
-  end
-
-  def filter(cellar, color, country: country, year: year) do
-    cellar
-    |> filter_by_color(color)
-    |> IO.inspect(label: "\nfilter by year and country: ")
-    |> filter_by_year(year)
-    |> IO.inspect(label: "\nfilter by year and country: ")
-    |> filter_by_country(country)
-    |> IO.inspect(label: "\nfilter by year and country: ")
-  end
-
-  def filter(cellar, color, _) do
-    cellar
-    |> filter_by_color(color)
-  end
-
-  defp filter_by_color([], color), do: []
-
-  defp filter_by_color([wine | rest], color) when elem(wine, 0) == color do
-    [elem(wine, 1) | filter_by_color(rest, color)]
-  end
-
-  defp filter_by_color([ _ | rest], color) do
-    filter_by_color(rest, color)
+    |> Keyword.get_values(color)
+    |> filter_by_year(opts[:year])
+    |> filter_by_country(opts[:country])
   end
 
   # The functions below do not need to be modified.
